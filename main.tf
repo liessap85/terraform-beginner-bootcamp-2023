62b7c24357c97783e091e70d7bc8bfd08cd97f61
liessap85 <85364900+liessap85@users.noreply.github.com>
2023-09-22 19:18:04 +0100

    #9 added random provider (#10)

--- main.tf
+++ main.tf
@@ -1,3 +1,29 @@
 # This is my first change
 
 # This here is my second change, from github this time!
+
+
+terraform {
+    required_providers {
+        random = {
+            source = "hashicorp/random"
+            version = "3.5.1"
+        }
+    }
+}
+
+provider "random" {
+    # Config options
+}
+
+resource "random_string" "bucket_name" {
+    length = 16
+    special = false
+    upper = false
+
+}
+
+
+output "random_bucket_name" {
+    value = random_string.bucket_name.id
+}
